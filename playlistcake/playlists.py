4c09ad8035265492be6ab8107a752fadd68fc507
steinitzu <steini90@gmail.com>
2016-09-21 17:38:20 -0400

    add content decorators to playlist module

--- playlistcake/playlists.py
+++ playlistcake/playlists.py
@@ -1,7 +1,9 @@
 from .spotifystuff import iterate_results, get_spotify
 from .util import get_limit, get_ids, iter_chunked
+from .genutils import content
 
 
+@content('playlists')
 def user_playlists(max_results=None):
     limit = get_limit(max_results, 50)
     s = get_spotify()
@@ -13,6 +15,7 @@ def user_playlists(max_results=None):
         max_results=max_results)
 
 
+@content('tracks')
 def playlists_tracks(playlists):
     """
     Given a list/generator of simplified playlist
